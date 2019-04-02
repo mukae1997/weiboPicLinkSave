cd6fbf27e162012899e31489ad5183421558cffa
mukae1997 <444652692@qq.com>
2019-04-02 16:05:15 +0800

    添加markdown代码选择; 添加下载功能（with prompt)

--- background.js
+++ background.js
@@ -3,3 +3,20 @@
       console.log("The color is green.");
     });
   });
+
+chrome.runtime.onMessage.addListener(
+    function(message, sender, sendResponse) {
+        switch(message.type) {
+            case "piclink":
+                imgurl = message.piclink;
+
+			    chrome.downloads.download({url:imgurl},function(downloadId){
+			        console.log("download begin, the downId is:" + downloadId);
+			    });
+			    sendResponse();
+                break;
+            default:
+                console.error("Unrecognised message: ", message);
+        }
+    }
+);
--- getlinks.js
+++ getlinks.js
@@ -10,27 +10,90 @@ document.addEventListener('DOMContentLoaded', function()
 
 
 $(document).ready(function(){
-  $(window).scroll(function(event){
-  	// console.log(event);
-	var scrollTop = $(window).scrollTop();
-	var weiboReloadOffset = 2000;
-	console.log(scrollTop);
-	var thisAnchorPoint = Math.floor(scrollTop / weiboReloadOffset);
-	if (thisAnchorPoint > lastScrollAnchor) {
-		// 说明滑过了7000像素，触发一次加载
-		console.log("加载了新微博");
-		mainFunc();
+  	$(window).scroll(function(event){
+	  	// console.log(event);
+		var scrollTop = $(window).scrollTop();
+		var weiboReloadOffset = 2000;
+		// console.log(scrollTop); // 显示滚过的距离
+		var thisAnchorPoint = Math.floor(scrollTop / weiboReloadOffset);
+		if (thisAnchorPoint > lastScrollAnchor) {
+			// 说明滑过了7000像素，触发一次加载
+			console.log("重新分析了页面。");
+			mainFunc();
 
-	}
-	lastScrollAnchor = thisAnchorPoint;
+		}
+		lastScrollAnchor = thisAnchorPoint;
 	});
 
+ 	// radio button : switch code
+	// 动态添加的元素需要用 on 注册，且$()内的元素不能是动态创建的。
+	$(document).on('change', '.wplarea :radio', function() {
+			// console.log("checked .")
+	        if (this.value == 'raw') {
+	            // console.log('raw');
+
+	            $(this).parent().find(".myp").each(function() {
+
+	            	var imgsrc = $(this).attr('src');
+	            	$(this).html(imgsrc);
+	            });
+	        }
+	        else if (this.value == 'Markdown') {
+	            // console.log('markdown');
+
+	            $(this).parent().find(".myp").each(function() {
+
+	            	var imgsrc = $(this).attr('src');
+	            	$(this).html("![]("+imgsrc+")");
+	            });
+	             
+	        }
+
+	        else if (this.value == '下载') {
+	            // console.log('markdown');
+
+	            $(this).parent().find(".myp").each(function() {
+
+	            	var imgsrc = $(this).attr('src');
+	            	// $(this).html("![]("+old+")");
+
+	            	var imgurl = imgsrc;
+				    chrome.extension.sendMessage({
+				    	type:'piclink',
+				    	piclink: imgsrc
+						}, function(response) {
+					    //code to initialize my extension
+					});
+	            });
+	             
+	        }
+	}); 
 });
 
+function AddNewRadio(name, type, parent) {
 
+	var newradio = document.createElement('input');
+	newradio.setAttribute("name", type);
+	newradio.setAttribute("type", "radio");
+	newradio.setAttribute("value", name);
+
+	var newlabel = document.createElement('label');
+	newlabel.innerHTML = name;
+	newlabel.setAttribute("style", "padding:5px;")
+
+	parent.appendChild(newradio);
+	parent.appendChild(newlabel);
+ 
+}
+function showPic() {
+	// TODO:
+}
 function mainFunc() {
 	var functionDiv = "<div class=\"wplarea\" style=\"padding: 20px;\"> WPL功能区 </div>";
  	// $(".WB_detail").append(); 
+
+
+
 	// createPicTip(); // 左上角创建一个 fixed 的区域用于鼠标移到连接上时展示图片
 
 	var nodes = document.getElementsByClassName('WB_detail');
@@ -57,10 +120,19 @@ function mainFunc() {
 			wpldiv.innerHTML = "WPL功能区";
 			wpldiv.setAttribute("style", "padding: 20px;");
 			wpldiv.setAttribute("class", "wplarea");
+
+			// add radio buttons 
+ 			
+			AddNewRadio("raw", "codetype", wpldiv);
+
+			AddNewRadio("Markdown", "codetype", wpldiv);
+			AddNewRadio("下载", "codetype", wpldiv);
+
 			e.appendChild(wpldiv);
 
 		}	
 
+ 
 		for (lni in linknodes) {
 			if (isNaN(parseInt(lni))) continue;
 			var ln = linknodes[lni];
@@ -78,9 +150,11 @@ function mainFunc() {
  			var tempId = ni.toString() + "-" + lni.toString();
 
 			pelement.setAttribute("class", "myp");
+			pelement.setAttribute("src", largetImgLink);
 			pelement.setAttribute("id", tempId);
+			// pelement.setAttribute("id", tempId);
 			// pelement.setAttribute("onmouseout","document.getElementById(\""+tempId+"\").style.display='none'")
-			pelement.setAttribute("onmouseover","showPic")
+			// pelement.setAttribute("onmouseover","showPic")
 			wpldiv.appendChild(pelement);
 
 			// console.log(largetImgLink);
@@ -97,8 +171,9 @@ function mainFunc() {
 		if (debugcnt > debugmax) return ;
 	}
 	lastAddIndex = nodes.length;
-}
 
+	
+}
 function down(src, name) {  
         // // 生成一个a元素
         // var a = document.createElement('a')
